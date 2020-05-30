bc2146b32ffbc72a7f31b5e4dfa6458bc51089e0
Tim Mineard <tim.mineard@yahoo.com>
2020-05-30 15:22:45 -0700

    Task 1-3

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Directory from './components/DirectoryComponent';
-import './App.css';
-import { CAMPSITES } from './shared/campsites';
+import React, { Component } from "react";
+import { Navbar, NavbarBrand } from "reactstrap";
+import Directory from "./components/DirectoryComponent";
+import { CAMPSITES } from "./shared/campsites";
+import "./App.css";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            campsites: CAMPSITES
-        };
-    }
-    render() {
-        return (
-            <div className="App">
-                <Navbar dark color="primary">
-                <div className="container">
-                    <NavbarBrand href="/">NuCamp</NavbarBrand>
-                </div>
-                </Navbar>
-                <Directory campsites={this.state.campsites} />
-            </div>
-        );
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			campsites: CAMPSITES,
+		};
+	}
+	render() {
+		return (
+			<div className='App'>
+				<Navbar dark color='primary'>
+					<div className='container'>
+						<NavbarBrand href='/'>NuCamp</NavbarBrand>
+					</div>
+				</Navbar>
+				<Directory campsites={this.state.campsites} />
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+
+class CampsiteInfo extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	renderCampsite(campsite) {
+		return (
+			<div className="col-md-5 m-1">
+				<Card>
+					<CardImg top src={campsite.image} alt={campsite.name} />
+					<CardBody>
+						<CardTitle>{campsite.name}</CardTitle>
+						<CardText>{campsite.description}</CardText>
+					</CardBody>
+				</Card>
+			</div>
+		);
+	}
+
+	renderComments(comments){
+		if(comments){
+			return (
+				<div className="col-md-5 m-1">
+					<h4>Comments</h4>
+					{
+						comments.map(c => {
+							return (
+								<div>
+									<p>{c.text}</p>
+									<p>{c.author} - {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(c.date)))}</p>
+								</div>
+							);
+					    })
+					}
+				</div>
+			);
+		}
+		return <div />;
+	}
+
+	render() {
+		if (this.props.campsite) {
+			return (
+				<div className='row'>
+					{this.renderCampsite(this.props.campsite)}
+					{this.renderComments(this.props.campsite.comments)}
+				</div>
+			);
+		}
+		return <div />;
+		
+	}
+}
+
+export default CampsiteInfo;
