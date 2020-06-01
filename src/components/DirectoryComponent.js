23a2ec505aa5e018d149efdc90a6a4eeb0a95cd8
Tim Mineard <tim.mineard@yahoo.com>
2020-06-01 10:32:05 -0700

    Functional Components

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,50 +1,31 @@
-import React, { Component } from "react";
+import React from "react";
 import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 
+function RenderDirectoryItem({ campsite, onClick }) {
+	return (
+		<Card onClick={() => onClick(campsite.id)}>
+			<CardImg width='100%' src={campsite.image} alt={campsite.name} />
+			<CardImgOverlay>
+				<CardTitle>{campsite.name}</CardTitle>
+			</CardImgOverlay>
+		</Card>
+	);
+}
 
-class Directory extends Component {
-
-	render() {
-		const directory = this.props.campsites.map(campsite => {
-			return (
-				<div key={campsite.id} className='col-md-5 m-1'>
-					<Card onClick={() => this.props.onClick(campsite.id)}>
-						<CardImg width='100%' src={campsite.image} alt={campsite.name} />
-						<CardImgOverlay>
-							<CardTitle>{campsite.name}</CardTitle>
-						</CardImgOverlay>
-					</Card>
-				</div>
-			);
-		});
-
+function Directory(props) {
+	const directory = props.campsites.map((campsite) => {
 		return (
-			<div className='container'>
-				<div className='row'>
-					{directory}
-				</div>
+			<div key={campsite.id} className='col-md-5 m-1'>
+				<RenderDirectoryItem campsite={campsite} onClick={props.onClick} />
 			</div>
 		);
-	}
+	});
+
+	return (
+		<div className='container'>
+			<div className='row'>{directory}</div>
+		</div>
+	);
 }
 
 export default Directory;
-//				<div className='row'>
-//					<div className='col-md-5 m-1'>
-//						{this.renderSelectedCampsite(this.state.selectedCampsite)}
-//					</div>
-//				</div>;
-//	renderSelectedCampsite(campsite) {
-//		if (campsite) {
-//			return (
-//				<Card>
-//					<CardImg top src={campsite.image} alt={campsite.name} />
-//					<CardBody>
-//						<CardTitle>{campsite.name}</CardTitle>
-//						<CardText>{campsite.description}</CardText>
-//					</CardBody>
-//				</Card>
-//			)
-//		}
-//		return <div />;
-//	}
