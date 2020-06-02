3424224414134894abd9c961e0dcb34ba0e4e28c
Tim Mineard <tim.mineard@yahoo.com>
2020-06-02 10:22:07 -0700

    React Router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React, { Component } from 'react';
 import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<Main />
-			</div>
+			<BrowserRouter>
+				<div className="App">
+					<Main />
+				</div>
+			</BrowserRouter>
 		);
-	};
+	}
 }
 
 export default App;
-
-//				<Navbar dark color='primary'>
-//					<div className='container'>
-//						<NavbarBrand href='/'>NuCamp</NavbarBrand>
-//					</div>
-//				</Navbar>
-//				<Directory campsites={this.state.campsites} />
